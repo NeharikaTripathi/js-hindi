4ad5d5b0e66bb64f87f5badfae650134f2051438
neharika <140141052+NeharikaTripathi@users.noreply.github.com>
2025-06-25 20:44:46 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,30 @@
+ let score = "neharika"
+
+ /*working on backend so you dont know the type
+ hence we need to find the type
+  */
+//  console.log(typeof score);//string
+//  console.log(typeof (score));//string
+
+ let valueInNumber = Number (score)//this guarentees that score has been converted to number
+// console.log(typeof valueInNumber);//number
+// console.log(valueInNumber);//"NaN"
+//"33" =>33
+//"33abc" =>NaN Not an Number
+//true =>1 ;false =>0
+
+let isLoggedIn = "neharika"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+//1 =>true ; 0=>false
+//"" => false
+//"neharika" =>true
+
+ let someNumber = 33
+ let stringNumber = String(someNumber)
+ console.log(typeof stringNumber);
+ 
+
+ 
