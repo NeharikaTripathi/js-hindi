3a47ea1db8377fc07624a2b9a0e8436e8cfa5012
neharika <140141052+NeharikaTripathi@users.noreply.github.com>
2025-06-25 19:21:22 +0000

    container config done

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -1,4 +1,17 @@
-console.log("Neharika")
-const accountID = 123456
-console.log(accountID)
+const accountID = 12456
+let accountEmail = "neharika@google.com"
+var accountPassword = "123556"
+ accountCity = "jaipur"
+
+ let accountState;
+ //accountID = 2// not allowed
+
+ accountEmail="ne@g.com"
+ accountPassword = "21343563463456w"
+ accountCity = "Hyderabad"
+
+
+ console.table([accountID,accountEmail,accountPassword,accountCity,accountState])
+
+
 
