c2e4772b28cfddd650e5c8d281e9ee05a23175fd
neharika <140141052+NeharikaTripathi@users.noreply.github.com>
2025-06-25 19:24:52 +0000

    changes

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -3,6 +3,10 @@ let accountEmail = "neharika@google.com"
 var accountPassword = "123556"
  accountCity = "jaipur"
 
+ /*
+ prefer not to use var
+ beacause of block scope and functional scope
+ */
  let accountState;
  //accountID = 2// not allowed
 
