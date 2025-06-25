c0d7dd5d5388315badd320e15fe37521f54c1ac7
neharika <140141052+NeharikaTripathi@users.noreply.github.com>
2025-06-25 21:01:47 +0000

    operations

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -16,7 +16,7 @@
 let isLoggedIn = "neharika"
 
 let booleanIsLoggedIn = Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn);
+//console.log(booleanIsLoggedIn);
 
 //1 =>true ; 0=>false
 //"" => false
@@ -24,7 +24,46 @@ console.log(booleanIsLoggedIn);
 
  let someNumber = 33
  let stringNumber = String(someNumber)
- console.log(typeof stringNumber);
+ //console.log(stringNumber)
+ //console.log(typeof stringNumber);
  
+//**************************** Operations ********************** 
+ 
+ let value = 3;
+ let negValue = -value
+ //console.log(negValue);
 
- 
+//  console.log(2+2);
+//  console.log(2-2)
+//  console.log(2*2);
+//  console.log(2**3)//power
+//  console.log(2/3)
+//  console.log(2%3);
+
+let str1 = "hello"
+let str2 = " neharika"
+
+let str3 = str1 + str2
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 +2);
+// console.log(1 + 2 +"2");
+
+// console.log(((3 + 4) * 5) % 3);
+// dont write ,not appreciated
+// console.log(+true);
+// console.log(+"");
+
+let num1 , num2 , num3
+
+num1 = num2 = num3 = 2 + 2
+
+let gameCounter = 100;
+//prefix and postfix operators 
+gameCounter++;
+console.log(gameCounter);
+
+
+
