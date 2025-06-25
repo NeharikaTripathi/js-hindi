4a71657dce03e69a89355bc2995088c23b22b47c
neharika <140141052+NeharikaTripathi@users.noreply.github.com>
2025-06-25 23:56:16 +0400

    changes done

--- 01_basics/02_datatypes.js
+++ 01_basics/02_datatypes.js
@@ -1,3 +1,26 @@
 "use strict"//treat all JS code as newer version
+//alert(3+3) //we are using node js not browser
+
+console.log(3+ 3)
+
+console.log("Neharika")
+
+let name ="neharika"
+let age ="18"
+let loggeIn = "false"
+
+let state = null//null value
+//primitive datatypes//
+//number => 2 to power 53
+//bigInt
+//string =>""
+//boolean => true/false
+//null=>standalone value=>does not mean empty
+//undefined => value is not assigned
+//symbol  =>unique
+
+//object
+
+console.console.log(typeof"Neharika");
 
 
