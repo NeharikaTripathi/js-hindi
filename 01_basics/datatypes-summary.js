43236a7b7ae6a7acf88f95e22c6ad2b01b03a9c2
neharika <140141052+NeharikaTripathi@users.noreply.github.com>
2025-06-26 20:26:01 +0000

    datatypeSummary

---
+++ 01_basics/datatypes-summary.js
@@ -0,0 +1,16 @@
+/*data types are basically divided into two parts
+ primitive and non-primitive 
+ //call by valu & call by reference(non-primitive) 
+ difference is based on how these data values are "stored in the mem
+ and how we can access those values (two categorisation)"
+*/
+//primitive(call by value)
+//7 types: String, Number, Boolean, null, undefined,Symbol,BigInt
+
+//JS is Dynamically typed lang or statically ==> dynamically
+//JS mei lang is not defined
+
+const score = 100;
+
+//Non-Primitive(Reference)(call by reference)
+//Array, Objects, Functions
