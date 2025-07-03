173af0ba0c6b3c2c96eab52aa8ec18847db6a4b1
neharika <140141052+NeharikaTripathi@users.noreply.github.com>
2025-07-03 08:23:06 +0000

    dates

--- 01_basics/07_datesinJS.js
+++ 01_basics/07_datesinJS.js
@@ -1,3 +1,5 @@
+//Date --> calculated in millisecond from 1st jan 1970
+
 let myDate = new Date();
 // console.log(myDate.toString());
 // console.log(myDate.toDateString());
@@ -5,6 +7,35 @@ let myDate = new Date();
 // console.log(typeof myDate);//object
 
 //let myCreatedDate = new Date(2025,0,23);//yyyy/m/dd
-let myCreatedDate = new Date(2025, 0, 23, 5, 3);
-console.log(myCreatedDate.toDateString());
-console.log(myCreatedDate.toLocaleString());
+//0->Jan 11->decemeber
+// let myCreatedDate = new Date(2025, 6, 3, 5, 3);
+// console.log(myCreatedDate.toDateString());//Thu Jul 03 2025
+// console.log(myCreatedDate.toLocaleString());//7/3/2025, 5:03:00 AM
+let myCreatedDate = new Date("01-14-2025");
+// console.log(myCreatedDate.toLocaleString());
+
+let myTimeStamp = Date.now();
+
+// console.log(myTimeStamp);
+// console.log(myCreatedDate.getTime());//gives the timestamp
+// console.log(Math.floor(Date.now()/1000));
+
+
+let newDate = new Date();
+console.log(newDate);
+console.log(newDate.getMonth()+1);
+console.log(newDate.getDay()+1);
+
+'${newDate.getDay()} and the time '
+//costomize in localeString function
+newDate.toLocaleString('default',{
+    weekday: "long"
+    
+})
+
+
+
+
+
+
+
